aff3052bfcca7d968ecc99f63fc5ad1546a5320d
DANNJUGUNA <ndan30071@gmail.com>
2023-04-06 15:45:56 +0300

    added the headers on the navigation

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import {BsFillHeartPulseFill} from 'react-icons/bs'
 const Navbar = () => {
   return (
-    <div className=' flex flex-row py-4' >
-        <div className=' mx-5 text-white flex flex-col'>
-            <BsFillHeartPulseFill size={45}/>
+    <nav className=' flex flex-row py-4 items-center bg-white' >
+        <div className=' mx-5 text-red-500 flex flex-col'>
+            <p className='ml-6'>< BsFillHeartPulseFill size={50}/></p>
             <p className=' font-thin'>A life saved is our happiness</p>
         </div>
-        <div>
+        <div className=' flex flex-row items-center text-red-500 justify-between font-semibold'>
             
+              <li>Home</li>
+              <li>About us</li>
+              <li>Dashboard</li>
+              <li>Sign up</li>
+              <li>Login</li>
+          
         </div>
-    </div>
+        <hr className=' bg-white'/>
+    </nav>
   )
 }
 
