431ee9110ffd5ad61a2ec70695a8f2a286b40295
DANNJUGUNA <ndan30071@gmail.com>
2023-04-06 16:22:11 +0300

    styled and added the functionality of the navbar buttons

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -3,22 +3,24 @@ import {Link} from 'react-router-dom'
 import {BsFillHeartPulseFill} from 'react-icons/bs'
 const Navbar = () => {
   return (
-    <nav className=' flex flex-row py-4 items-center bg-white' >
-        <div className=' mx-5 text-red-500 flex flex-col'>
-            <p className='ml-6 flex flex-row items-center font-semibold text-black '>Safe <span className='text-red-500 ml-1 mr-1'>< BsFillHeartPulseFill size={50}/></span> Life</p>
-            <p className=' font-thin text-black'>A life saved is our happiness</p>
+    <div>
+    <nav className=' flex flex-row flex-wrap py-4 items-center bg-white pb-1 gap-52' >
+        <div className=' ml-12 text-red-500 flex flex-col mr-16'>
+            <p className='ml-6 flex flex-row items-center font-semibold text-black text-xl'>Safe <span className='text-red-500 ml-1 mr-1'>< BsFillHeartPulseFill size={50}/></span> Life</p>
+            <p className=' font-thin text-[#BF2222]'>A life saved is our happiness</p>
         </div>
-        <div className=' flex flex-row items-center text-red-500 justify-between font-semibold'>
+        <div className=' text-[#BF2222] font-semibold flex flex-wrap justify-between gap-6 mr-5'>
             
-              <li>Home</li>
-              <li>About us</li>
-              <li>Dashboard</li>
-              <li>Sign up</li>
-              <li>Login</li>
+              <Link to='/'>Home</Link >
+              <Link to='/about'>About us</Link>
+              <Link to='/dashboard'>Dashboard</Link>
+              <Link to='/signup'>Sign up</Link>
+              <Link to='login'>Login</Link>
           
         </div>
         <hr className=' bg-white'/>
     </nav>
+    </div>
   )
 }
 
