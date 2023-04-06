1a5653dbb9b9073096341be36d2a467a9173816f
DANNJUGUNA <ndan30071@gmail.com>
2023-04-06 20:24:58 +0300

    added images on the home page

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='flex flex-row py-20 mx-6'>
+      <img className='rounded-full w-[300px] h-[300px] bg-cover'
+      src="https://images.pexels.com/photos/5149400/pexels-photo-5149400.jpeg?auto=compress&cs=tinysrgb&w=400" alt="" />
+       <img className='rounded-full w-[300px] h-[300px] bg-cover'
+        src="https://images.pexels.com/photos/4531306/pexels-photo-4531306.jpeg?auto=compress&cs=tinysrgb&w=400" alt="" />
+      <img className='rounded-full w-[300px] h-[300px] bg-cover'
+      src="https://media.istockphoto.com/id/1316973706/photo/3d-rendered-illustration-of-blood-drop-isolated-on-white.jpg?b=1&s=170667a&w=0&k=20&c=M1J0ZZaoDSLV8F4CLeHvBllmdh1lBijmDcaLObjpvHU=" alt="" />
+      <img className='rounded-full w-[300px] h-[300px] bg-cover'
+      src="https://images.unsplash.com/photo-1536856136534-bb679c52a9aa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Ymxvb2QlMjBkb25hdGlvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
+    </div>
   )
 }
 
