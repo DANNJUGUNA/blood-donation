c52241287961986effc9ee56580d64622c53e222
DANNJUGUNA <ndan30071@gmail.com>
2023-04-06 16:47:52 +0300

    updated the style

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -4,12 +4,12 @@ import {BsFillHeartPulseFill} from 'react-icons/bs'
 const Navbar = () => {
   return (
     <div>
-    <nav className=' flex flex-row flex-wrap py-4 items-center bg-white pb-1 gap-52' >
+    <nav className=' flex flex-row flex-wrap py-4 items-center bg-white pb-1 gap-12' >
         <div className=' ml-12 text-red-500 flex flex-col mr-16'>
-            <p className='ml-6 flex flex-row items-center font-semibold text-black text-xl'>Safe <span className='text-red-500 ml-1 mr-1'>< BsFillHeartPulseFill size={50}/></span> Life</p>
+            <p className='ml-6 flex flex-row items-center font-semibold text-black text-2xl'>Safe <span className='text-red-500 ml-1 mr-1'>< BsFillHeartPulseFill size={50}/></span> Life</p>
             <p className=' font-thin text-[#BF2222]'>A life saved is our happiness</p>
         </div>
-        <div className=' text-[#BF2222] font-semibold flex flex-wrap justify-between gap-6 mr-5'>
+        <div className=' text-[#BF2222] font-semibold flex flex-wrap justify-between gap-9 mr-5 text-xl'>
             
               <Link to='/'>Home</Link >
               <Link to='/about'>About us</Link>
