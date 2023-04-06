85aa97321d2e2955d36c6f71a3c89717cf80873e
DANNJUGUNA <ndan30071@gmail.com>
2023-04-06 16:05:52 +0300

    added the routes to the application

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,24 @@
 import react from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Layout from './components/layout/Layout';
-
+import Home from './pages/home/Home'
+import Aboutus from './pages/about/Aboutus'
+import DashBoard from './pages/dashboard/DashBoard'
+import SignUp from './pages/signup/SignUp'
+import Login from './pages/login/Login'
 function App() {
   return (
-    <div >
-      <Layout/>
-    </div>
+    <BrowserRouter>
+       <Routes>
+            <Route path='/' element={<Layout/>}>
+                 <Route index element={<Home/>}/>
+                 <Route path='/about' element={<Aboutus/>}/>
+                 <Route path='/dashboard' element={<DashBoard/>}/>
+                 <Route path='/signup' element={<SignUp/>}/>
+                 <Route path='/login' element={<Login/>}/>
+            </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
