23dfa797f8983cc262e75a5376264ddbe9aa5fa6
DANNJUGUNA <ndan30071@gmail.com>
2023-04-06 14:24:46 +0300

    added the log on the navbar

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
-
+import {BsFillHeartPulseFill} from 'react-icons/bs'
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className=' flex flex-row py-4' >
+        <div className=' mx-5 text-white flex flex-col'>
+            <BsFillHeartPulseFill size={45}/>
+            <p>A life saved is our happiness</p>
+        </div>
+    </div>
   )
 }
 
