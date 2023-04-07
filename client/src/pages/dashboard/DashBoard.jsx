c3d4fd8a01ceeaf4aa2063fd108b99148f86651c
DANNJUGUNA <ndan30071@gmail.com>
2023-04-07 12:57:14 +0300

    justified the items center

--- client/src/pages/dashboard/DashBoard.jsx
+++ client/src/pages/dashboard/DashBoard.jsx
@@ -4,7 +4,7 @@ import{SiSemaphoreci} from 'react-icons/si'
 import {MdOutlineEventAvailable,MdEventSeat} from 'react-icons/md'
 const DashBoard = () => {
   return (
-    <div className='flex flex-row mt-5 gap-5 mx-9'>
+    <div className='flex flex-row mt-5 gap-9 items-center justify-center '>
       <div className=' bg-white h-[100px] w-[180px] flex items-center justify-center shadow-slate-300 shadow-2xl rounded-3xl flex-col hover:scale-105 duration-300'>
       <BiDonateBlood size={45} className='text-red-500'/>
         <p className=' font-semibold text-xl'>Donate Blood</p>
