77d3a1a0190c8f94d654d1f496f81cb5fceb9bb3
DANNJUGUNA <ndan30071@gmail.com>
2023-04-07 12:51:22 +0300

    updated the icon

--- client/src/pages/dashboard/DashBoard.jsx
+++ client/src/pages/dashboard/DashBoard.jsx
@@ -1,22 +1,26 @@
 import React from 'react'
 import {BiDonateBlood} from 'react-icons/bi'
 import{SiSemaphoreci} from 'react-icons/si'
-import {MdOutlineEventAvailable} from 'react-icons/md'
+import {MdOutlineEventAvailable,MdEventSeat} from 'react-icons/md'
 const DashBoard = () => {
   return (
     <div className='flex flex-row mt-5 gap-5 mx-9'>
-      <div className=' bg-white h-[100px] w-[180px] flex items-center justify-center shadow-slate-300 shadow-2xl rounded-3xl flex-col'>
+      <div className=' bg-white h-[100px] w-[180px] flex items-center justify-center shadow-slate-300 shadow-2xl rounded-3xl flex-col hover:scale-105 duration-300'>
       <BiDonateBlood size={45} className='text-red-500'/>
         <p className=' font-semibold text-xl'>Donate Blood</p>
        
       </div>
-      <div className=' bg-white h-[100px] w-[180px] flex items-center justify-center rounded-3xl flex-col  shadow-slate-300 shadow-2xl'>
+      <div className=' bg-white h-[100px] w-[180px] flex items-center justify-center rounded-3xl flex-col  shadow-slate-300 shadow-2xl hover:scale-105 duration-300'>
         <SiSemaphoreci size={45} className='text-red-500'/>
         <p className=' font-semibold text-xl'>Request Blood</p>
       </div>
-      <div className=' bg-white h-[100px] w-[180px] flex items-center justify-center  shadow-slate-300 shadow-2xl rounded-3xl flex-col'>
+      <div className=' bg-white h-[100px] w-[180px] flex items-center justify-center  shadow-slate-300 shadow-2xl rounded-3xl flex-col hover:scale-105 duration-300'>
         <MdOutlineEventAvailable size={45} className='text-red-500'/>
         <p className='font-semibold text-xl'>Avaible Blood</p>
+      </div>
+      <div className=' bg-white h-[100px] w-[180px] flex items-center justify-center  shadow-slate-300 shadow-2xl rounded-3xl flex-col hover:scale-105 duration-300'>
+        <MdEventSeat size={45} className='text-red-500'/>
+        <p className='font-semibold text-xl '>Events</p>
       </div>
     </div>
   )
