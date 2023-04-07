0bd7190575f38ec48ccba5ef90a088298d88f4b4
DANNJUGUNA <ndan30071@gmail.com>
2023-04-07 12:24:06 +0300

    styled the a component

--- client/src/pages/dashboard/DashBoard.jsx
+++ client/src/pages/dashboard/DashBoard.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 
 const DashBoard = () => {
   return (
-    <div className='flex flex-row mt-5 gap-5'>
-      <div className=' bg-white h-[100px] w-[150px] flex items-center justify-center'>
-        <p>Donate Blood</p>
+    <div className='flex flex-row mt-5 gap-5 mx-9'>
+      <div className=' bg-white h-[100px] w-[150px] flex items-center justify-center shadow-slate-300 shadow-2xl rounded-3xl'>
+        <p className=' font-semibold text-xl'>Donate Blood</p>
+        
       </div>
       <div className=' bg-white h-[100px] w-[150px] flex items-center justify-center'>
         <p>Request Blood</p>
