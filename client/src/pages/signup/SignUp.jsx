81de1106046d9a9ba9e8a6a157f727ad5b9d105a
DANNJUGUNA <ndan30071@gmail.com>
2023-04-07 11:37:56 +0300

    updated the code

--- client/src/pages/signup/SignUp.jsx
+++ client/src/pages/signup/SignUp.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import MovingText from 'react-moving-text'
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
+    <div>
+     <MovingText
+      type="flipSlowDown"
+      duration="1000ms"
+      delay="0s"
+      direction="normal"
+      timing="ease"
+      iteration="infinite"
+      fillMode="none">
+      Content...
+    </MovingText>
+  </div>
   )
 }
 
