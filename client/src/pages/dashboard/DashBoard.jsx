6cbea0eb48898a6a146a605c2cdc1b553397be6e
DANNJUGUNA <ndan30071@gmail.com>
2023-04-07 12:21:24 +0300

    added dashboard components

--- client/src/pages/dashboard/DashBoard.jsx
+++ client/src/pages/dashboard/DashBoard.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const DashBoard = () => {
   return (
-    <div>DashBoard</div>
+    <div className='flex flex-row mt-5 gap-5'>
+      <div className=' bg-white h-[100px] w-[150px] flex items-center justify-center'>
+        <p>Donate Blood</p>
+      </div>
+      <div className=' bg-white h-[100px] w-[150px] flex items-center justify-center'>
+        <p>Request Blood</p>
+      </div>
+      <div className=' bg-white h-[100px] w-[150px] flex items-center justify-center'>
+        <p>Avaible Blood</p>
+      </div>
+    </div>
   )
 }
 
