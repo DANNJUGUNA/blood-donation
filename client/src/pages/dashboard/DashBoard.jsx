5a16b1e909697f5e5ef7f425fd606d300f2adb61
DANNJUGUNA <ndan30071@gmail.com>
2023-04-07 12:28:37 +0300

    updated the style

--- client/src/pages/dashboard/DashBoard.jsx
+++ client/src/pages/dashboard/DashBoard.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-
+import {BiDonateBlood} from 'react-icons/bi'
+import{SiSemaphoreci} from 'react-icons/si'
 const DashBoard = () => {
   return (
     <div className='flex flex-row mt-5 gap-5 mx-9'>
-      <div className=' bg-white h-[100px] w-[150px] flex items-center justify-center shadow-slate-300 shadow-2xl rounded-3xl'>
+      <div className=' bg-white h-[100px] w-[180px] flex items-center justify-center shadow-slate-300 shadow-2xl rounded-3xl flex-col'>
+      <BiDonateBlood size={40} className='text-red-500'/>
         <p className=' font-semibold text-xl'>Donate Blood</p>
-        
+       
       </div>
       <div className=' bg-white h-[100px] w-[150px] flex items-center justify-center'>
         <p>Request Blood</p>
