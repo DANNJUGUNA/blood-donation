31bb7c7ec795d42d06b1a70887345221cb48cc61
DANNJUGUNA <ndan30071@gmail.com>
2023-04-08 15:07:12 +0300

    updated the style

--- client/src/pages/dashboard/DashBoard.jsx
+++ client/src/pages/dashboard/DashBoard.jsx
@@ -5,22 +5,22 @@ import {MdOutlineEventAvailable,MdEventSeat} from 'react-icons/md'
 const DashBoard = () => {
   return (
     <div className='flex flex-row mt-5 gap-9 items-center justify-center '>
-      <div className=' bg-white h-[100px] w-[180px] flex items-center justify-center shadow-slate-300 shadow-2xl rounded-3xl flex-col hover:scale-105 duration-300'>
+      <div className=' bg-cyan-500 h-[100px] w-[180px] flex items-center justify-center shadow-black shadow-xl rounded-3xl flex-col hover:scale-105 duration-300'>
       <BiDonateBlood size={45} className='text-red-500'/>
-        <p className=' font-semibold text-xl'>Donate Blood</p>
+        <p className=' font-semibold text-xl text-white'>Donate Blood</p>
        
       </div>
-      <div className=' bg-white h-[100px] w-[180px] flex items-center justify-center rounded-3xl flex-col  shadow-slate-300 shadow-2xl hover:scale-105 duration-300'>
+      <div className=' bg-cyan-500 h-[100px] w-[180px] flex items-center justify-center rounded-3xl flex-col  shadow-black shadow-xl hover:scale-105 duration-300'>
         <SiSemaphoreci size={45} className='text-red-500'/>
-        <p className=' font-semibold text-xl'>Request Blood</p>
+        <p className=' font-semibold text-xl text-white'>Request Blood</p>
       </div>
-      <div className=' bg-white h-[100px] w-[180px] flex items-center justify-center  shadow-slate-300 shadow-2xl rounded-3xl flex-col hover:scale-105 duration-300'>
+      <div className=' bg-cyan-500 h-[100px] w-[180px] flex items-center justify-center  shadow-black shadow-xl rounded-3xl flex-col hover:scale-105 duration-300'>
         <MdOutlineEventAvailable size={45} className='text-red-500'/>
-        <p className='font-semibold text-xl'>Avaible Blood</p>
+        <p className='font-semibold text-xl text-white'>Avaible Blood</p>
       </div>
-      <div className=' bg-white h-[100px] w-[180px] flex items-center justify-center  shadow-slate-300 shadow-2xl rounded-3xl flex-col hover:scale-105 duration-300'>
+      <div className=' bg-cyan-500 h-[100px] w-[180px] flex items-center justify-center  shadow-black shadow-xl rounded-3xl flex-col hover:scale-105 duration-300'>
         <MdEventSeat size={45} className='text-red-500'/>
-        <p className='font-semibold text-xl '>Events</p>
+        <p className='font-semibold text-xl  text-white'>Events</p>
       </div>
     </div>
   )
